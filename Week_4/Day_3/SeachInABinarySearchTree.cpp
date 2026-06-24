01357e99b0e69633f4312f6c9f136dab6f7b2f4a
Mahwish  Ali Naaz <mahwishali2005@gmail.com>
2026-06-24 00:58:27 -0700

    Create SeachInABinarySearchTree.cpp

---
+++ Week_4/Day_3/SeachInABinarySearchTree.cpp
@@ -0,0 +1,23 @@
+class Solution {
+public:
+    TreeNode* searchBST(TreeNode* root, int val)
+{
+        if(root==NULL){
+            return NULL;
+        }
+        if(root->val==val)
+{
+            return root;
+        }
+        if(val<root->val)
+{
+           return searchBST(root->left,val);
+        }
+        if(val>root->val)
+{
+           return searchBST(root->right,val);
+        }
+        return NULL;
+        
+    }
+};
